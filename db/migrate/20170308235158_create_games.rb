397435851845f6c279b778133f23fda8af959ec9
Jordan Munoz <jordantmunoz@gmail.com>
2017-03-08 20:48:04 -0800

    adds updates to database, schema migration

--- db/migrate/20170308235158_create_games.rb
+++ db/migrate/20170308235158_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      t.string :location, null: false
+      t.string  :location, null: false
       t.datetime :time, null: false
       t.boolean :home_team, null: false
       t.string :opposing_team, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170309033305) do
 
-ActiveRecord::Schema.define(version: 20170309031832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "confirmations", force: :cascade do |t|
+    t.integer  "player_id",  null: false
+    t.integer  "game_id",    null: false
+    t.boolean  "confirmed",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", force: :cascade do |t|
     t.string   "location",      null: false
