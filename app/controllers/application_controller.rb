84aadbb64cfaec02133783d71dc1a20062da9d1f
tommy <thomaskclark@gmail.com>
2017-03-08 21:39:54 -0800

    Cleanup David's mess; remove current_user helper - test okay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user
-
-  def current_user
-  	@current_user ||= User.find_by(id: session[:user_id])
-  end
-
-  protected
+protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
