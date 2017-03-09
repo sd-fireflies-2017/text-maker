4fdddff540875aed705216f779a81b5ecdca181b
Jordan Munoz <jordantmunoz@gmail.com>
2017-03-09 09:33:45 -0800

    updates associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,9 @@ class User < ApplicationRecord
   has_many :games, through: :teams
   has_many :players, through: :teams
 
+  # scope :upcoming_games, -> { games.take(5) }
+  def upcoming_games
+  	 games.all.take(5)
+  end
 
 end
