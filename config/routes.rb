d1f76fd403a7996b4e36f36d447d2a4b39832c1a
David McLaughlin <davidmclaugh@gmail.com>
2017-03-08 17:31:23 -0800

    Add team#new, team#show, user#show, game#new, and game#show views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'visitors#index'
+  devise_for :users
+  resources :users
+
+  resources :players
+
+  resources :teams do 
+  	resources :games
+  	resources :players
+  end
 end
